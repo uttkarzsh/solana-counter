489133474296f5bec9c92452c3469f0b2de7cc02
Uttkarsh Pawan <utkbest@gmail.com>
2025-11-06 01:20:01 +0530

    counter program logic

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -3,14 +3,62 @@ use anchor_lang::prelude::*;
 declare_id!("9hjmtXpw32DwY61S5GkmDSLgRYyFuz2JUa7rgB3x6Xvx");
 
 #[program]
-pub mod counter {
+pub mod counter_program {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Decrement>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count -= 1;
+        Ok(())
+    }
+
+    pub fn set_count(ctx: Context<SetCount>, value: u64) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = value;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[derive(Accounts)]
+pub struct SetCount<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
